1e11ee591f5c10337f81905a8922dfe32407c2d6
Epenko1337 <43629089+Epenko1337@users.noreply.github.com>
2024-02-01 18:15:43 +0500

    Add files via upload

---
+++ ieee80211.h
@@ -0,0 +1,28 @@
+#ifndef __IEE80211_H__
+#define __IEE80211_H__
+#endif
+
+#include "c_types.h"
+
+#if defined(NONOSDK305)
+#define NONOSDK (0x30500)
+#else
+#define NONOSDK (0x22100)
+#endif
+
+#define FREEDOM_OUTPUT_INVALID_INTERFACE -4
+#define FREEDOM_OUTPUT_INVALID_ARGS -3
+#define FREEDOM_OUTPUT_NOT_ALLOWED -2
+#define FREEDOM_OUTPUT_ALLOC_FAILED -1
+#define FREEDOM_OUTPUT_SUCCESS 0 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int ieee80211_freedom_output(uint8 dinterface, uint8* buf, uint32 len, bool seq_ctl);
+
+
+#ifdef __cplusplus
+}
+#endif
